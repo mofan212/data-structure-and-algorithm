7f95b4381bb2f4609d7c0bfeb3df4b84901425c6
mofan <cy.mofan@foxmail.com>
2022-11-27 22:09:39 +0800

    refactor: 最长公共子串的进一步优化

--- _08-动态规划/src/indi/mofan/LCSubstring.java
+++ _08-动态规划/src/indi/mofan/LCSubstring.java
@@ -25,12 +25,41 @@ public class LCSubstring {
         }
         int[] dp = new int[rowsChars.length + 1];
         int max = 0;
+
+        for (int i = 1; i <= closChars.length; i++) {
+            // 从大到下递减
+            for (int j = rowsChars.length; j >= 1; j--) {
+                if (closChars[i - 1] != rowsChars[j - 1]) {
+                    dp[j] = 0;
+                } else {
+                    dp[j] = dp[j - 1] + 1;
+                    max = Math.max(dp[j], max);
+                }
+            }
+        }
+        return max;
+    }
+
+    static int lcs2(String str1, String str2) {
+        if (str1 == null || str2 == null) return 0;
+        char[] chars1 = str1.toCharArray();
+        if (chars1.length == 0) return 0;
+        char[] chars2 = str2.toCharArray();
+        if (chars2.length == 0) return 0;
+
+        char[] closChars = chars1, rowsChars = chars2;
+        if (chars1.length < chars2.length) {
+            closChars = chars2;
+            rowsChars = chars1;
+        }
+        int[] dp = new int[rowsChars.length + 1];
+        int max = 0;
         for (int i = 1; i <= closChars.length; i++) {
             int cur = 0;
             for (int j = 1; j <= rowsChars.length; j++) {
                 int leftTop = cur;
                 cur = dp[j];
-                if (closChars[i  - 1] != rowsChars[j - 1]) {
+                if (closChars[i - 1] != rowsChars[j - 1]) {
                     dp[j] = 0;
                 } else {
                     dp[j] = leftTop + 1;
