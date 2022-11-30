526cf46274cd32826ae5094957d30616ae3d0925
mofan <cy.mofan@foxmail.com>
2022-11-30 22:14:27 +0800

    feat: 二分搜索模板

--- _01-排序/src/com/yang/BinarySearch.java
+++ _01-排序/src/com/yang/BinarySearch.java
@@ -1,5 +1,8 @@
 package com.yang;
 
+import java.util.function.BiFunction;
+import java.util.function.Predicate;
+
 /**
  * @author 默烦
  * @date 2020/8/13
@@ -41,5 +44,42 @@ public class BinarySearch {
             }
         }
         return begin;
+    }
+
+    /**
+     * 根据指定条件，使用二分搜索获取目标元素的下标
+     *
+     * @param array       数组
+     * @param isBlue      中值判断条件
+     * @param returnValue 返回值
+     * @param <T>         数组类型
+     * @return 目标元素下标
+     */
+    public static <T> int binarySearchTemplate(T[] array,
+                                               Predicate<T> isBlue,
+                                               BiFunction<Integer, Integer, Integer> returnValue) {
+        if (array == null || array.length == 0) return -1;
+        int l = -1, r = array.length;
+        while (l + 1 != r) {
+            int m = (l + r) >> 1;
+            if (isBlue.test(array[m])) {
+                l = m;
+            } else {
+                r = m;
+            }
+        }
+        return returnValue.apply(l, r);
+    }
+
+    public static void main(String[] args) {
+        Integer[] array = {1, 2, 3, 5, 5, 5, 8, 9};
+        // 第一个大于等于 5 的元素 --> 3
+        System.out.println(binarySearchTemplate(array, i -> i < 5, (l, r) -> r));
+        // 最后一个小于 5 的元素 --> 2
+        System.out.println(binarySearchTemplate(array, i -> i < 5, (l, r) -> l));
+        // 第一个大于 5 的元素 --> 6
+        System.out.println(binarySearchTemplate(array, i -> i <= 5, (l, r) -> r));
+        // 最后一个大于等于 5 的元素 --> 5
+        System.out.println(binarySearchTemplate(array, i -> i <= 5, (l, r) -> l));
     }
 }
